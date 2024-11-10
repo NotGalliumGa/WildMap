c5457c035a35cb8c061323cd475f158c7d65f12f
Diego Luis Dela Cruz <diegoluisdc05@gmail.com>
2024-11-09 21:25:28 -0500

    added new mailing code to server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -7,65 +7,39 @@ const app = express();
 
 app.use(express.urlencoded());
 
-app.get('/', function(request, response, next){
-
-	response.send(`
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-		<div class="container">
-			<h1 class="text-center mt-3 mb-3">Submit Form Data in Node.js</h1>
-			<div class="card">
-				<div class="card-header">Sample Form</div>
-				<div class="card-body">
-					<form method="POST" action="/">
-						<div class="mb-3">
-							<label>First Name</label>
-							<input type="text" name="first_name" id="first_name" class="form-control" />
-						</div>
-						<div class="mb-3">
-							<label>Last Name</label>
-							<input type="text" name="last_name" id="last_name" class="form-control" />
-						</div>
-						<div class="mb-3">
-		                	<label>Email Address</label>
-		                	<input type="text" name="email_address" id="email_address" class="form-control" />
-		                </div>
-		                <div class="mb-3">
-		                	<input type="submit" name="submit_button" class="btn btn-primary" value="Add" />
-		                </div>
-					</form>
-				</div>
-			</div>
-		</div>
-	`);
-
-
+app.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../FrontEnd/index.html'));
 });
 
-app.post('/', function(request, response, next){
-    var data=request.body;
+app.post('/', function(req, res, next){
+  var data=req.body;
 
-    var smtpTransport = nodemailer.createTransport({
-       host: "smtp.gmail.com", 
-       auth: {
-       user: "diegoluisdc05@gmail.com",
-       pass: "nikk nmkc ucvd usqa"
-       }});
+  const name = req.body.name;
+  const location_desc = req.body.location_desc;
+  const general_desc = req.body.general_desc;
 
-   smtpTransport.sendMail({  //email options
-   from: "diegoluisdc50@gmail.com",
-   to: "galven.rivera@gmail.com", // receiver
-   subject: "Emailing with nodemailer", // subject
-   html: "here your data goes" // body (var data which we've declared)
-    }, function(error, response){  //callback
-         if(error){
-           console.log(error);
-        }else{
-           console.log("Message sent: " + response.message);
-       }
+  var smtpTransport = nodemailer.createTransport({
+     host: "smtp.gmail.com", 
+     auth: {
+     user: "diegoluisdc05@gmail.com",
+     pass: "nikk nmkc ucvd usqa"
+     }});
 
-   smtpTransport.close(); 
-    }); 
+     smtpTransport.sendMail({
+      from: "diegoluisdc50@gmail.com",
+      to: "diegoluisdc05@gmail.com",
+      subject: "Emailing with nodemailer",
+      text: `New form submission:\n\nName: ${name}\n\nlocation_desc: ${location_desc}\n\ngeneral_desc: ${general_desc}`
+  }, function(error, info) {  // Renamed parameter to avoid conflict
+      if (error) {
+          console.error("Error:", error);
+          res.status(500).send("Failed to send email.");
+      } else {
+          console.log("Message sent:", info.messageId);
+          res.send("Email sent successfully!");
+      }
+      smtpTransport.close();
+  });
 
 });
 
